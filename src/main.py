440ad237dcfa5826d58a7a984e57605d2d20f682
Lusine Vasilyan <vasilyanlusin@gmail.com>
2024-07-23 14:40:44 +0400

    made a server using FastAPI framework

--- src/main.py
+++ src/main.py
@@ -87,22 +87,6 @@ def get_all_molecules():
 
 from rdkit import Chem
 
-def substructure_search(mols, mol):
-    
-    sub_mol = Chem.MolFromSmiles(mol) # defining molecule and substructure to search for
-    
-    if sub_mol is None: 
-        raise ValueError("No substructure")
-    
-    matching_molecules = []
-
-    for smiles in mols:
-        molecule = Chem.MolFromSmiles(smiles)
-        if molecule is not None and molecule.HasSubstructMatch(sub_mol):
-            matching_molecules.append(smiles)
-
-
-    return matching_molecules
 
 @app.post("/molecules/search")
 def search_substructure(substructure: str):
